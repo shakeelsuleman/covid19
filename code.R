ecdde82faf00cda94474395f72e2a15fec57b9b3
shakeelsuleman <shakeel.suleman011940@yahoo.com>
2020-03-29 15:28:35 +0100

    updates

--- code.R
+++ code.R
@@ -2,6 +2,7 @@ library(utils)
 library(httr)
 library(ggplot2)
 library(gganimate)
+library(readxl)
 
 GET("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv", authenticate(":", ":", type="ntlm"), write_disk(tf <- tempfile(fileext = ".csv")))
 #read the Dataset sheet into “R”. The dataset will be called "data".
@@ -19,3 +20,14 @@ world<- ggplot(data = data, aes(x = dates, y = cases, fill = countriesAndTerrito
   geom_col(show.legend = FALSE)
 
 world<- world + labs(title = "Country:{closest_state}")
+
+download.file("https://fingertips.phe.org.uk/documents/Historic%20COVID-19%20Dashboard%20Data.xlsx", "cov.xlsx", mode = "wb")
+
+dh<- read_xlsx("cov.xlsx", sheet = 3, skip = 6)
+
+
+
+
+
+
+
--- readme.md
+++ readme.md
@@ -1,7 +1,9 @@
-** Covid-19 ECDC data **
+# Covid-19 ECDC data
 
 * [Data download from ECDC Website](https://www.ecdc.europa.eu/en/publications-data/download-todays-data-geographic-distribution-covid-19-cases-worldwide)
 
 * [WHO Covid19 Situation Reports](https://www.who.int/emergencies/diseases/novel-coronavirus-2019/situation-reports)
 
 * [Covid-19 Guiance for health professionals](https://www.gov.uk/government/collections/wuhan-novel-coronavirus)
+
+* [COVID-19: track coronavirus cases](https://www.gov.uk/government/publications/covid-19-track-coronavirus-cases)
